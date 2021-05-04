f1ba43e9eba334a006c6ac856255b19a9a860e58
Mykej <68675481+Mykej@users.noreply.github.com>
2021-05-04 07:03:51 -0500

    Updated MongoError: Authentication failed
    -> Changed userName and password and added a new user in MongoDB database access tab.
    -> Changed module export mongoURI to not use parentheses.
    -> Everything working properly <-

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
-const db = require('./config/keys').mongoURI; //import key/connection string
 const express = require('express');//This helps create a new express server.
+const bodyParser = require('body-parser');
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
 
 const app = express(); //this helps creates a new express server.
+const db = require('./config/keys').mongoURI; //import key/connection string
 
 //connect to MongoDB using mongoose:
 mongoose
@@ -16,9 +19,16 @@ mongoose
 
 //setup basic route so that we can render some information on our page. 
 app.get("/", (req, res) => res.send("Hello World!!!"));
+//Tell express to use your newly imported routes(put them with your 'Hello world' route)
+app.use('/api/users', users);
+app.use("/api/tweets", tweets);
+
+//setup middleware for body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //Before we can run the server, we need to tell our app which port to run on. Keeping in mind that we will later be deploying our app to Heroku, which requires us to run our server on:
-const port = process.env.PORT || 5000; //locally our server will run on :localHhost: 5000
+const port = process.env.PORT || 5000; //locally our server will run on :localhost: 5000
 
 //Finally, let's tell Express to start a socket and listen for connections on the path.
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
