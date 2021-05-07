8669c76ada7bd8eb2602e08398b46a2d1dd346ec
Mykej <68675481+Mykej@users.noreply.github.com>
2021-05-07 14:53:36 -0500

    Fixed 500 Error
    -> Got rid of body-parser and used app.use(express.urlencoded({extended: true})).

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const express = require('express');//This helps create a new express server.
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
+const User = require('./models/User');//imported newSchema
 
 const app = express(); //this helps creates a new express server.
 const db = require('./config/keys').mongoURI; //import key/connection string
+
+app.use(express.urlencoded({extended: true}));//respond to request from other software
 
 //connect to MongoDB using mongoose:
 mongoose
@@ -18,14 +21,20 @@ mongoose
     .catch(err => console.log(err));
 
 //setup basic route so that we can render some information on our page. 
-app.get("/", (req, res) => res.send("Hello World!!!"));
+app.get("/", (req, res) => {
+    const user = new User({
+        handle: "myke",
+        email: "myke@myke.myke",
+        password: "myke"
+    })
+    user.save()
+    res.send("Hello World!!!")});
 //Tell express to use your newly imported routes(put them with your 'Hello world' route)
 app.use('/api/users', users);
 app.use("/api/tweets", tweets);
 
-//setup middleware for body-parser
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+//setup middleware
+
 
 //Before we can run the server, we need to tell our app which port to run on. Keeping in mind that we will later be deploying our app to Heroku, which requires us to run our server on:
 const port = process.env.PORT || 5000; //locally our server will run on :localhost: 5000
--- users.js
+++ users.js
@@ -3,16 +3,17 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 
+
 router.get('/test', (req, res) => {
     res.json({msg: "This is the users route"})
 });
 router.post('/register', (req, res) => {
     // check to make sure nobody has already registered with a duplicate email
-    User.findOne({email: req.body.email })
+    User.findOne({ email: req.body.email })
     .then(user => {
         if (user) {
             // Throw a 400 error if the email address already exists
-            res.status(400).json({email: "A user has already registered with that address."})
+            return res.status(400).json({email: "A user has already registered with this address."})
         } else {
             //Otherwise create a new user
             const newUser = new User({
@@ -27,12 +28,33 @@ router.post('/register', (req, res) => {
                     if (err) throw err;
                     newUser.password = hash;
                     newUser.save()
-                        .then(user => res.json(user))
+                        .then((user) => res.json(user))
                         .catch(err => console.group(err));
                 })
             })
+            res.status(200).console.log({msg: "You are updated"});
         }
     })
 })
+// router.post('/login', (req, res) => {
+//     const email = req.body.email;
+//     const password = req.body.password;
+
+//     User.findOne({email})
+//     .then(user => {
+//         if(!user){
+//             res.status(404).json({email: "This user does not exist"});
+//         }
+
+//         bcrypt.compare(password, user.password)
+//         .then(isMatch => {
+//             if(isMatch) {
+//                 res.json({msg: 'Success'});
+//             } else {
+//                 res.status(400).json({password: 'Incorrect Password'});
+//             }
+//         })
+//     })
+// })
 
 module.exports = router;
