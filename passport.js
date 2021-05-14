b395d5c4aa4fd67d200120a1b3f8a3d96dc79529
Mykej <68675481+Mykej@users.noreply.github.com>
2021-05-14 14:30:10 -0500

    401 error fixed
    -> line 14 - typo | User and not user

--- passport.js
+++ passport.js
@@ -11,7 +11,7 @@ options.secretOrKey = keys.secretOrKey;
 module.exports = passport => {
     //arguments: use ceretain strategy which takes two arguments, options we created line 7 and a call back. callback takes two arguments, 1: jwt_payload: items we have specified... and done which is a keyword/passes data to frontend.
     passport.use(new JwtStrategy(options, (jwt_payload, done) => {
-        user.findById(jwt_payload.id)
+        User.findById(jwt_payload.id)
         .then(user => {
             if(user) {
                 //return the user to the frontend
